53f6b9c461bb05154802f162905dab1da9e2d396
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-12-29 11:00:12 -0600

    Empezando clase 97

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import TeoriaRedux from './components/TeoriaRedux';
 import { Provider } from 'react-redux';
 import store from './store';
+import Contador from './components/Contador';
 
 function App() {
 	return (
 		<Provider store={store}>
 			<div className="App" style={{ textAlign: 'center' }}>
 				<h1>Redux</h1>
+				<Contador />
 				<hr />
 				<TeoriaRedux />
 			</div>
--- src/types/index.js
+++ src/types/index.js
@@ -1,5 +1,12 @@
 //Acciones para el contador
 
+//Contador
+export const INCREMENT = "INCREMENT"
+export const DECREMENT = "DECREMENT"
+export const INCREMENT_5 = "INCREMENT_5"
+export const DECREMENT_5 = "DECREMENT_5"
+export const RESET = "RESET"
+
 //Shopping cart
 
 //Crud
